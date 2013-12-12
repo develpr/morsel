a4d66adb91d07f068fff276cef3be5e94d82179f
develpr <kevin@develpr.com>
2013-12-11 22:48:06 -0800

    Update seeds to make user id primary

--- app/database/seeds/RecipientSenderTableSeeder.php
+++ app/database/seeds/RecipientSenderTableSeeder.php
@@ -7,59 +7,59 @@ class RecipientSenderTableSeeder extends Seeder {
         DB::table('recipient_sender')->delete();
 
 		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 1, 'sender_id' => 2)
-		);
-
-		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 3, 'sender_id' => 2)
-		);
-
-		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 4, 'sender_id' => 2)
-		);
-
-		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 5, 'sender_id' => 2)
-		);
-
-		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 6, 'sender_id' => 2)
-		);
-
-		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 7, 'sender_id' => 2)
-		);
-
-		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 8, 'sender_id' => 2)
-		);
-
-		DB::table('recipient_sender')->insert(
 			array('recipient_id' => 2, 'sender_id' => 1)
 		);
 
 		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 2, 'sender_id' => 3)
+			array('recipient_id' => 3, 'sender_id' => 1)
 		);
 
 		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 2, 'sender_id' => 4)
+			array('recipient_id' => 4, 'sender_id' => 1)
 		);
 
 		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 2, 'sender_id' => 5)
+			array('recipient_id' => 5, 'sender_id' => 1)
 		);
 
 		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 2, 'sender_id' => 6)
+			array('recipient_id' => 6, 'sender_id' => 1)
 		);
 
 		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 2, 'sender_id' => 7)
+			array('recipient_id' => 7, 'sender_id' => 1)
 		);
 
 		DB::table('recipient_sender')->insert(
-			array('recipient_id' => 2, 'sender_id' => 8)
+			array('recipient_id' => 8, 'sender_id' => 1)
+		);
+
+		DB::table('recipient_sender')->insert(
+			array('recipient_id' => 1, 'sender_id' => 2)
+		);
+
+		DB::table('recipient_sender')->insert(
+			array('recipient_id' => 1, 'sender_id' => 3)
+		);
+
+		DB::table('recipient_sender')->insert(
+			array('recipient_id' => 1, 'sender_id' => 4)
+		);
+
+		DB::table('recipient_sender')->insert(
+			array('recipient_id' => 1, 'sender_id' => 5)
+		);
+
+		DB::table('recipient_sender')->insert(
+			array('recipient_id' => 1, 'sender_id' => 6)
+		);
+
+		DB::table('recipient_sender')->insert(
+			array('recipient_id' => 1, 'sender_id' => 7)
+		);
+
+		DB::table('recipient_sender')->insert(
+			array('recipient_id' => 1, 'sender_id' => 8)
 		);
 
     }
